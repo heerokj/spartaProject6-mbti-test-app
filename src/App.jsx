2016db0d67cc755de48ad5f6cc3233c924b5e019
heerokj <jungheerok@gmail.com>
2024-09-07 17:08:24 +0900

    Authcontext 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./context/AuthContext";
 import Router from "./shared/Router"; //오류 :import 시 내가 만든걸로!!
 
 //오류: 생성하고 주입을 안했었음
-const queryClient = new QueryClient(); //오류 : useQuer ~~ 이걸로 사용했음..
+// const queryClient = new QueryClient(); //오류 : useQuer ~~ 이걸로 사용했음..
 const App = () => {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
+      {/* <QueryClientProvider client={queryClient}> */}
+      <AuthProvider>
         <Router />
-      </QueryClientProvider>
+      </AuthProvider>
+      {/* </QueryClientProvider> */}
     </>
   );
 };
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,25 @@
 //FIXME - 인증된 유저의 정보를 가지고있는 context
 
-import React from "react";
+import { createContext, useEffect, useState } from "react";
 
-const AuthContext = () => {
-  return <div>AuthContext</div>;
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  //인증을 관리하는 상태
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  //인증이 되었는지 판단기준 : localStorage에 accessToken이름의 item이 존재하는가
+  //localStorage에 접근해서 토큰이 존재하는지 담기
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={isAuthenticated}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
-
-export default AuthContext;
